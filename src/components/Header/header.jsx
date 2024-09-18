d655072acb000b51cd2f32445b39c953c7382ce6
Korvin <hofilenakorvin@gmail.com>
2024-09-18 05:34:22 -0700

    Added header.jsx

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,5 +1,25 @@
-function Header() {
-  return <header>Header</header>;
+import React from "react";
+
+function Header({ weather }) {
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <header className="header">
+      <div className="header__logo">WTWR</div>
+      <div className="header__date">{currentDate}</div>
+      <div className="header__location">
+        {weather ? weather.city : "Loading..."}
+      </div>
+      <button className="header__add-button">Add Clothes</button>
+      <div className="header__user">
+        <img className="header__avatar" src="avatar.png" alt="User Avatar" />
+        <span className="header__name">Redux</span>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
