2260ab529b44c6854a7dc2e0f2345830fd22bcc2
Korvin <hofilenakorvin@gmail.com>
2024-10-08 04:11:44 -0700

    added contexts folder

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.css";
-import { CurrentTemperatureUnitProvider } from "../src/CurrentTemperatureUnitContext.js";
+import { CurrentTemperatureUnitProvider } from "./contexts/CurrentTemperatureUnitContext"; // Corrected path
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
