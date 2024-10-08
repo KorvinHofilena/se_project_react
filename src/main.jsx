0a85a10392476c700db1806d1089e829b8c22b73
Korvin <hofilenakorvin@gmail.com>
2024-10-08 04:24:30 -0700

    Wrapped app with browser router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.css";
-import { CurrentTemperatureUnitProvider } from "./contexts/CurrentTemperatureUnitContext"; // Corrected path
+import { CurrentTemperatureUnitProvider } from "./contexts/CurrentTemperatureUnitContext"; // Ensure this path is correct
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CurrentTemperatureUnitProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </CurrentTemperatureUnitProvider>
   </React.StrictMode>
 );
