81a9b729ace0e1182da768cea318f898cc9b2860
Korvin <hofilenakorvin@gmail.com>
2024-10-08 04:01:49 -0700

    adjusted Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.css";
+import { CurrentTemperatureUnitProvider } from "./contexts/CurrentTemperatureUnitContext"; // Import the provider
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <CurrentTemperatureUnitProvider>
+      <App />
+    </CurrentTemperatureUnitProvider>
   </React.StrictMode>
 );
