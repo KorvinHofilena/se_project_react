257597e0f2678f66fc2c44c93efd13be728c92da
Korvin <hofilenakorvin@gmail.com>
2025-02-06 02:38:01 -0800

    adjusted auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,40 +5,54 @@ const baseUrl =
   "http://localhost:3001";
 
 export async function signUserIn(data) {
-  const res = await fetch(`${baseUrl}/signin`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
+  try {
+    const res = await fetch(`${baseUrl}/signin`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-  const result = await res.json();
-  if (!res.ok) {
-    throw new Error(result.message || "Error during sign in");
+    const result = await res.json();
+    if (!res.ok) {
+      throw new Error(result.message || "Error during sign in");
+    }
+
+    return result;
+  } catch (error) {
+    console.error("Sign-in error:", error);
+    throw error;
   }
-
-  return result;
 }
 
 export async function signUserUp(data) {
-  const res = await fetch(`${baseUrl}/signup`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
+  try {
+    const res = await fetch(`${baseUrl}/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-  const result = await res.json();
-  if (!res.ok) {
-    throw new Error(result.message || "Error during sign up");
+    const result = await res.json();
+    if (!res.ok) {
+      throw new Error(result.message || "Error during sign up");
+    }
+
+    return result;
+  } catch (error) {
+    console.error("Sign-up error:", error);
+    throw error;
   }
-
-  return result;
 }
 
 export async function fetchUserData(token) {
+  if (!token) {
+    throw new Error("Token is missing");
+  }
+
   try {
     const response = await fetch(`${baseUrl}/users/me`, {
       headers: {
@@ -46,6 +60,7 @@ export async function fetchUserData(token) {
         "Content-Type": "application/json",
       },
     });
+
     const data = await response.json();
     if (!response.ok) {
       throw new Error(data.message || "Error fetching user data");
