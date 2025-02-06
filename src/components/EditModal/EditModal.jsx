f9a176e4dab24564a618d1a86a338fd345762c75
Korvin <hofilenakorvin@gmail.com>
2025-02-06 03:27:02 -0800

    added EditModal.jsx/css

---
+++ src/components/EditModal/EditModal.jsx
@@ -0,0 +1,110 @@
+import { useState, useEffect, useContext } from "react";
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import CurrentUserContext from "../../context/CurrentUserContext";
+import "./EditModal.css";
+
+const EditProfileModal = ({ closeActiveModal, isOpen, handleEditProfile }) => {
+  const currentUser = useContext(CurrentUserContext);
+  const [name, setName] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [isButtonActive, setIsButtonActive] = useState(false);
+  const [errors, setErrors] = useState({ name: "", avatar: "" });
+
+  const validateForm = () => {
+    let isValid = true;
+    let errors = {
+      name: "",
+      avatar: "",
+    };
+
+    if (!name.trim()) {
+      errors.name = "Name is required.";
+      isValid = false;
+    }
+
+    if (!avatar.trim()) {
+      errors.avatar = "Image URL is required.";
+      isValid = false;
+    } else if (!/^https?:\/\/.+/.test(avatar)) {
+      errors.avatar = "Invalid URL format.";
+      isValid = false;
+    }
+
+    setErrors(errors);
+    return isValid;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      handleEditProfile({ name, avatar });
+    }
+  };
+
+  const handleNameChange = (e) => setName(e.target.value || "");
+  const handleAvatarChange = (e) => setAvatar(e.target.value || "");
+
+  useEffect(() => {
+    if (name.trim() && avatar.trim()) {
+      setIsButtonActive(true);
+    } else {
+      setIsButtonActive(false);
+    }
+  }, [name, avatar]);
+
+  useEffect(() => {
+    if (isOpen && currentUser) {
+      setName("" || currentUser.name);
+      setAvatar("" || currentUser.avatar);
+      setErrors({ name: "", avatar: "" });
+    }
+  }, [isOpen, currentUser]);
+
+  return (
+    <ModalWithForm
+      title="Change Profile Data"
+      buttonText="Save Changes"
+      buttonClass={`modal__submit ${
+        isButtonActive ? "modal__submit_active" : ""
+      }`}
+      isOpen={isOpen}
+      onClose={closeActiveModal}
+      onSubmit={handleSubmit}
+      name={"editprofile"}
+    >
+      <button
+        className="modal__close"
+        type="button"
+        onClick={closeActiveModal}
+      />
+      <label className="modal__label">
+        Name *
+        <input
+          type="text"
+          className="modal__input"
+          id="name"
+          placeholder="Name"
+          value={name}
+          onChange={handleNameChange}
+          required
+        />
+        {errors.name && <span className="modal__error">{errors.name}</span>}
+      </label>
+      <label className="modal__label">
+        Avatar *
+        <input
+          type="url"
+          className="modal__input"
+          id="avatar"
+          placeholder="Image URL"
+          value={avatar}
+          onChange={handleAvatarChange}
+          required
+        />
+        {errors.avatar && <span className="modal__error">{errors.avatar}</span>}
+      </label>
+    </ModalWithForm>
+  );
+};
+
+export default EditProfileModal;
