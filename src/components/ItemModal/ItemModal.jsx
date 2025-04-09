ffd9ecca793927b067c726f0fa510de0e12700b5
Korvin <hofilenakorvin@gmail.com>
2025-04-09 04:08:36 -0700

    adjusted ItemModal.jsx

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,92 +1,42 @@
-import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import "./ItemModal.css";
-import { useState } from "react";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
-function AddItemModal({ isOpen, closeActiveModal, handleAddItemSubmit }) {
-  const [name, setName] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
-  const [weather, setWeather] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    handleAddItemSubmit({ name, imageUrl, weather }, () => {
-      setName("");
-      setImageUrl("");
-      setWeather("");
-    });
-  };
+function ItemModal({ activeModal, card, onClose, handleDeleteClick }) {
+  const currentUser = useContext(CurrentUserContext);
 
   return (
-    <ModalWithForm
-      title="New Garment"
-      name="add-garment"
-      buttonText="Add Garment"
-      isOpen={isOpen}
-      onClose={closeActiveModal}
-      onSubmit={handleSubmit}
-      formValid={true} // <-- TEMP FIX until validation is implemented
-    >
-      <label className="modal__label">
-        Name
-        <input
-          type="text"
-          className="modal__input"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Name"
-          required
+    <div className={`modal ${activeModal === "preview" ? "modal_opened" : ""}`}>
+      <div className="modal__content item-modal__content_type_image">
+        <button
+          className="modal__close"
+          type="button"
+          onClick={onClose}
+        ></button>
+        <img
+          src={card?.imageUrl}
+          alt={card?.name}
+          className="item-modal__image"
         />
-      </label>
+        <div className="item-modal__footer">
+          <div className="item-modal__footer-text">
+            <h2 className="item-modal__caption">{card?.name}</h2>
+            <p className="item-modal__weather">Weather: {card?.weather}</p>
+          </div>
 
-      <label className="modal__label">
-        Image
-        <input
-          type="url"
-          className="modal__input"
-          value={imageUrl}
-          onChange={(e) => setImageUrl(e.target.value)}
-          placeholder="Image URL"
-          required
-        />
-      </label>
-
-      <label className="modal__label">
-        Select the weather type:
-        <div className="modal__radio-group">
-          <label className="modal__radio-label">
-            <input
-              type="radio"
-              value="hot"
-              checked={weather === "hot"}
-              onChange={(e) => setWeather(e.target.value)}
-              className="modal__radio"
-            />
-            Hot
-          </label>
-          <label className="modal__radio-label">
-            <input
-              type="radio"
-              value="warm"
-              checked={weather === "warm"}
-              onChange={(e) => setWeather(e.target.value)}
-              className="modal__radio"
-            />
-            Warm
-          </label>
-          <label className="modal__radio-label">
-            <input
-              type="radio"
-              value="cold"
-              checked={weather === "cold"}
-              onChange={(e) => setWeather(e.target.value)}
-              className="modal__radio"
-            />
-            Cold
-          </label>
+          {card?.owner === currentUser?._id && (
+            <button
+              type="button"
+              className="item-modal__delete-btn"
+              onClick={handleDeleteClick}
+            >
+              Delete Item
+            </button>
+          )}
         </div>
-      </label>
-    </ModalWithForm>
+      </div>
+    </div>
   );
 }
 
-export default AddItemModal;
+export default ItemModal;
