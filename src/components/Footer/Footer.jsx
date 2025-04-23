e8eed71df8ac36970dd1faabb347b3d41033945c
Korvin <hofilenakorvin@gmail.com>
2025-04-23 03:17:41 -0700

    adjusted footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import "./Footer.css";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="footer__container">
       <p className="footer__text">Developed by Korvin Hofilena</p>
-      <p className="footer__text">2024</p>
+      <p className="footer__text">{2024}</p>
     </footer>
   );
 }
