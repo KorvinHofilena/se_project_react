e44e1fc5df9507acc0a86f6f5103c73dd33f75d2
Korvin <hofilenakorvin@gmail.com>
2025-04-25 02:29:19 -0700

    added back ItemModal.jsx

---
+++ src/components/ItemModal/ItemModal.jsx
@@ -0,0 +1,36 @@
+import "./ItemModal.css";
+import close from "../../assets/CloseButton.png";
+import { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+
+function ItemModal({ activeModal, onClose, card, onDelete }) {
+  const currentUser = useContext(CurrentUserContext);
+
+  if (!card) return null;
+
+  const isOwn = currentUser?.currentUser?._id === card?.owner;
+  return (
+    <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
+      <div className="modal__content modal__content_type_image">
+        <button onClick={onClose} type="button" className="modal__close">
+          <img src={close} alt="close" />
+        </button>
+        <img src={card.imageUrl} alt={card.name} className="modal__image" />
+        <div className="modal__footer">
+          <h2 className="modal__caption">{card.name}</h2>
+          <p className="modal__weather">Weather: {card.weather}</p>
+          {isOwn && (
+            <button
+              className="modal__delete-card"
+              onClick={() => onDelete(card._id)}
+            >
+              Delete item
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ItemModal;
